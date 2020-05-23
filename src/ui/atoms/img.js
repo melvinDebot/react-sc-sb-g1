2b6e22b29d4e6f498fb530105b5a2126b284cb8c
lucram <lucienramassamy@gmail.com>
2020-05-23 14:59:27 +0200

    modificationd e sprobleme lie a l'atoms img

--- src/ui/atoms/img.js
+++ src/ui/atoms/img.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'props-type';
-import defaultBackground from '../../assets/img/Large';
+// import PropTypes from 'props-type';
+import defaultBackground from '../../assets/img/Thumbnail.png';
 
 export const photo = {
   imageBg : defaultBackground
