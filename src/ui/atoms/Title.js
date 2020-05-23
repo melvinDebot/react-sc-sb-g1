5d41a5d5a6ced601071238074decc7286bbab494
melvinDebot <melvindebot@gmail.com>
2020-05-23 21:49:02 +0200

    change title v2

--- src/ui/atoms/Title.js
+++ src/ui/atoms/Title.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import FontSize from '../particles/FontSize';
 
 export const sizes = {
@@ -8,22 +8,19 @@ export const sizes = {
   desc : "12px"
 }
 
-const Title = (textColor = "white", text) => {
-  const TitleColor = styled.h1`
-    color = ${textColor}
-    margin:0;
-    margin-top:5px;
-    padding:0;
-    font-size: ${sizes.desc}
-  `
-  return (
-    <TitleColor>{text}</TitleColor>
-  )
+const Texte = styled.h1`
+  color: white;
+  margin:0;
+  padding:0;
+  font-size: ${sizes.title}
+`
+
+const Title = props => {
+  const {size, text} = props;
+  return <Texte size={size}>{text}</Texte>
 }
 
-Title.propTypes = {
-  textColor: PropTypes.string
-};
+
 
 
 export default Title;
